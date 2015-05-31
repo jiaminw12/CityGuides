121c51741db8c412e76fd8b9a56184f33120a60b
jiaminw12 <jiaminw@ymail.com>
2015-06-01 01:54:10 +0800

    Updated login - only registered user can login

--- src/com/orbital/cityguide/Login.java
+++ src/com/orbital/cityguide/Login.java
@@ -1,10 +1,19 @@
 package com.orbital.cityguide;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,10 +25,18 @@ public class Login extends Activity {
 	Button clickHere;
 	Button login;
 
-	EditText Username;
-	EditText Password;
+	EditText userName;
+	EditText pw;
 
 	String title, alertboxmsg;
+	int success;
+	
+	private static final String LOGIN_URL = "http://192.168.1.7/City_Guide/login.php";
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
+	
+	// JSON parser class
+	JSONParser jsonParser = new JSONParser();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,24 +59,56 @@ public class Login extends Activity {
 		});
 
 		login = (Button) findViewById(R.id.login);
-		Username = (EditText) findViewById(R.id.username);
-		Password = (EditText) findViewById(R.id.password);
+		userName = (EditText) findViewById(R.id.username);
+		pw = (EditText) findViewById(R.id.password);
 
 		login.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				
+				String username = userName.getText().toString();
+				String password = pw.getText().toString();
+				
+				if (username.matches("") || password.matches("")) {
+					title = "Error Message";
+					alertboxmsg = "Please fill in all information needed.";
+					popupMessage(title, alertboxmsg);
+				} else {
+					try {
+						List<NameValuePair> params = new ArrayList<NameValuePair>();
+						params.add(new BasicNameValuePair("username", username));
+						params.add(new BasicNameValuePair("password", password));
 
-				title = "Message";
-				alertboxmsg = "Login successfully!";
-				popupMessage(title, alertboxmsg);
+						StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+								.permitAll().build();
+						StrictMode.setThreadPolicy(policy);
 
-				// go to eventList.java
-				Intent nextActivity = new Intent(Login.this,
-						AfterLoginNavigationList.class);
-				// Bundle extras = new Bundle();
-				// extras.putString("profile_username", username);
-				// nextActivity.putExtras(extras);
-				startActivity(nextActivity);
+						JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL,
+								"POST", params);
+						if (json != null) {
+							success = json.getInt(TAG_SUCCESS);
+							if (success == 1) {
+								title = "Message";
+								alertboxmsg = "Login successfully!";
+								popupMessage(title, alertboxmsg);
+
+								// go to eventList.java
+								Intent nextActivity = new Intent(Login.this,
+										AfterLoginNavigationList.class);
+								Bundle extras = new Bundle();
+								extras.putString("profile_username", username);
+								nextActivity.putExtras(extras);
+								startActivity(nextActivity);
+							} else if (success == 0) {
+								title = "Message";
+								alertboxmsg = "Invalid Credentials!";
+								popupMessage(title, alertboxmsg);
+							}
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		});
 	}
